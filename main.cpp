1560541010d201ac47cee9c2b2d4376d1be58594
unknown <marwanmarwan536@gmail.com>
2022-12-11 23:07:25 +0200

    Insert basic code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include "utils.cpp"
-#include "BTree.cpp"
+
 
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+    char* f ="Test.txt";
+    CreateIndexFile(f,10,10);
+    InsertNewRecordAtIndex(f,2,987);
+    InsertNewRecordAtIndex(f,5,127);
+    InsertNewRecordAtIndex(f,1,47);
+    InsertNewRecordAtIndex(f,7,3457);
+    InsertNewRecordAtIndex(f,19,454);
     return 0;
 }
